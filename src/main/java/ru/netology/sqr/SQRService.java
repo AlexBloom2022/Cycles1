b44d5252a9ad0fba84fac3f74424cb32585eced4
aleksandraurchenko <alex_bloom@mail.ru>
2022-03-24 17:08:42 +0300

    Fixed

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -5,7 +5,7 @@ public class SQRService {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
             if ((i * i <= upper) & (i * i >= lower)) {
-                counter = counter+1;
+                counter = counter + 1;
             }
         }
         System.out.println(counter);
